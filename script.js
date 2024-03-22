a128cc73447936dc1512ea140317917548af450f
jfmonsa <juanmonsalve@protonmail.com>
2024-03-22 00:38:39 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const proposal = document.getElementById("container");
+//const triggerBtn = document.getElementById("trigger");
+
+
+function show(){
+    document.getElementById("container").style.display = 'block';
+    console.log("trin");
+}
